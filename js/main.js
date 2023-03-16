27f5c662ac5cdc727c0f656d07a96eddaa54a177
Sofia <sofia.jespersen@hotmail.com>
2023-03-16 15:20:52 +0100

    text structure

--- js/main.js
+++ js/main.js
@@ -2,20 +2,37 @@ let contentWrapper = document.getElementById("contentWrapper")
 let homeBtn = document.getElementById("homeBtn");
 let portfolioBtn = document.getElementById("portfolioBtn");
 let cvBtn = document.getElementById("cvBtn");
+let contactBtn = document.getElementById("contactBtn");
 
+
+function loadPage() {
+    contentWrapper.innerHTML = `
+        <h1 class="first-page-heading">Frontend<br>developer</h1>
+        <p class="subheading">Frontend student, based in Stockholm.</p>`
+}
 
 homeBtn.addEventListener("click", () => {
-    closeMenu()
-    contentWrapper.innerHTML = `<h1>Sofia Jespersen</h1>`
-
+    closeMenu();
+    loadPage();
 })
 
 portfolioBtn.addEventListener("click", () => {
-    closeMenu()
+    closeMenu();
     contentWrapper.innerHTML = `<h1>Portfolio comming soon</h1>`
 })
 
 cvBtn.addEventListener("click", () => {
-    closeMenu()
+    closeMenu();
     contentWrapper.innerHTML = `<h1>CV comming soon</h1>`
 })
+
+contactBtn.addEventListener("click", () => {
+    closeMenu();
+    contentWrapper.innerHTML = `<h1>Contact comming soon</h1>`
+
+})
+
+
+
+
+loadPage();
