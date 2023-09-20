f2f4c74405d5aefd3f871bc2b0660ac268fa099f
SofiaJe <sofia.jespersen@hotmail.com>
2023-09-20 20:35:53 +0200

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "./",
   plugins: [react()],
   build: {
     outDir: 'docs', // Change 'docs' to 'public' if you prefer that directory name
