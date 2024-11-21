040f9c5d98f04221fff7cbfc4d33469342ba3900
Sofia <sofia.jespersen@hotmail.com>
2024-11-21 14:18:44 +0100

    temporarily

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,4 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   base: '/',
   plugins: [vue()],
-  // resolve: {
-  //   alias: {
-  //     'vue': 'vue/dist/vue.esm-bundler.js'
-  //   }
-  // }
 })
